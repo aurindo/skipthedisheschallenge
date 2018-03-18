50de13efef741411c3a9f74c1750be6765615fb7
Aurindo Jorge <aurindo@gmail.com>
2018-03-18 15:34:36 -0300

    First test

---
+++ challengeService/src/main/java/com/skipthediches/challenge/service/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.skipthediches.challenge.service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+//    public Use
+
+}
