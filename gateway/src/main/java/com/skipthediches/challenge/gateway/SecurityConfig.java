56600f5dd9b9d87c3234f74325fc6b3ee276e084
Aurindo Jorge <aurindo@gmail.com>
2018-04-21 17:57:43 -0300

    Add basic security between Discovery and Config. Start security config on Gateway

---
+++ gateway/src/main/java/com/skipthediches/challenge/gateway/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.skipthediches.challenge.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}password")
+                .roles("USER").and().withUser("admin").password("{noop}admin")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers("/challenge-service/**").permitAll().
+                antMatchers("/order-service/**").permitAll().
+                antMatchers("/discovery/**").hasRole("ADMIN").
+                    anyRequest().authenticated().and().formLogin().and().
+                logout().permitAll().
+                logoutSuccessUrl("/book-service/books")
+                    .permitAll().
+                and().csrf().disable();
+    }
+}
---
+++ gateway/src/main/java/com/skipthediches/challenge/gateway/SessionConfig.java
@@ -0,0 +1,11 @@
+package com.skipthediches.challenge.gateway;
+
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@EnableRedisHttpSession(
+        redisFlushMode = RedisFlushMode.IMMEDIATE)
+public class SessionConfig
+        extends AbstractHttpSessionApplicationInitializer {
+}
