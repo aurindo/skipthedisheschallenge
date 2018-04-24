7671af5fd45017fb7011c2ba04446d5fa35cef05
Aurindo Jorge <aurindo@gmail.com>
2018-04-23 22:54:59 -0300

    Add circuit breakers on order-service

--- orderService/src/main/java/com/skipthediches/challenge/service/OrderServiceApplication.java
+++ orderService/src/main/java/com/skipthediches/challenge/service/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.skipthediches.challenge.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
